5eaf97e381183b21d6914f0799954fa29ce9873f
Christopher Johannsen <chrisbjohannsen@gmail.com>
2016-12-19 09:54:38 -0800

    adding some additional quotes and integrated lodash to pick a random one

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 'use strict';
 
+let _ = require('lodash');
+
 console.log('Loading function');
 
 exports.handler = (event, context) => {
 	let responseCode = 200;
+	let quote = _.sample([
+		"No gnews, is good gnews, unless it's Gary's Gnews!",
+		"This is Gary Gnu... and the no gnews is good gnews show. The ONLY tv gnews show guar-an-TEED to contain NO gnews what-so-ever.",
+		"No gnews is good gnews with Gary Gnu"]);
+
     let responseBody = {
     	response_type: "in_channel",
-        text: "No Gnus is good gnus with Gary Gnu",
+        text: quote,
         attachments: [],
         //input: event
     };
