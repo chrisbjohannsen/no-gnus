54c9d59bfe2d0c6bd257eac5167d18eeb7d31699
Christopher Johannsen <chrisbjohannsen@gmail.com>
2016-12-19 09:43:29 -0800

    fixing the output so that all members of a channel can see it

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ console.log('Loading function');
 exports.handler = (event, context) => {
 	let responseCode = 200;
     let responseBody = {
-        message: "No Gnus is good gnus with Gary Gnu",
-        input: event
+    	response_type: "in_channel",
+        text: "No Gnus is good gnus with Gary Gnu",
+        attachments: [],
+        //input: event
     };
 
     let response = {
         statusCode: responseCode,
-        headers: {
-        	"Content-Type" : "text/plain"
-        },
-        body: responseBody.message
+        headers: {},
+        body: JSON.stringify(responseBody)
     };
     
     console.log("response : " + JSON.stringify(response));
